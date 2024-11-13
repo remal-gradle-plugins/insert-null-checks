eb57642f488ce356ed89b08f3b3228ea2bdb18fe
remal <remal@users.noreply.github.com>
2024-11-13 01:39:07 +0000

    Template cleanup

--- src/main/java/name/remal/gradle_plugins/insert_null_checks/TemplatePlugin.java
+++ src/main/java/name/remal/gradle_plugins/insert_null_checks/TemplatePlugin.java
@@ -1,4 +1,4 @@
-package name.remal.gradle_plugins.template;
+package name.remal.gradle_plugins.insert_null_checks;
 
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
---
+++ src/main/java/name/remal/gradle_plugins/insert_null_checks/package-info.java
@@ -0,0 +1,4 @@
+@NonNullApi
+package name.remal.gradle_plugins.insert_null_checks;
+
+import org.gradle.api.NonNullApi;
